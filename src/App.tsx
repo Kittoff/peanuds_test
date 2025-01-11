1e24f59c9fb1e29cbfbd502751fb3dade279c9f8
Kittoff <lozano.christophe@protonmail.com>
2025-01-11 11:02:29 +0100

    feat: added Tanstack React Query

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,18 @@ import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import People from './pages/People';
 import Films from './pages/Films/Films.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 const App = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/people"
-        element={<People />}
-      />
-      <Route
-        path="/films"
-        element={<Films />}
-      />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/films" element={<Films />} />
+      </Routes>
+    </QueryClientProvider>
   );
 };
 
